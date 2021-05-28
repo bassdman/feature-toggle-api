f83b19ec633a4f3d6b2beeeb019281af10745088
Manuel Gelsen <manuel.gelsen@gmail.com>
2021-05-28 22:00:44 +0200

    updated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const { series, src, dest } = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
@@ -7,7 +6,6 @@ const rollup = require('gulp-rollup');
 
 function build_module() {
     return src('src/index.js')
-        //      .pipe(sourcemaps.init())
         .pipe(rollup({
             // any option supported by Rollup can be set here.
             input: './src/index.js',
@@ -19,13 +17,11 @@ function build_module() {
             presets: ["@babel/preset-env"]
         }))
         .pipe(concat('feature-toggle-api.module.js'))
-        //     .pipe(sourcemaps.write())
         .pipe(dest('./'));
 };
 
 function build_raw() {
     return src('src/*.js')
-        //      .pipe(sourcemaps.init())
         .pipe(rollup({
             // any option supported by Rollup can be set here.
             input: './src/index.cjs.js',
@@ -37,13 +33,11 @@ function build_raw() {
             presets: ["@babel/preset-env"]
         }))
         .pipe(concat('feature-toggle-api.js'))
-        //     .pipe(sourcemaps.write())
         .pipe(dest('./'));
 };
 
 function build_min() {
     return src('src/*.js')
-        //   .pipe(sourcemaps.init())
         .pipe(rollup({
             // any option supported by Rollup can be set here.
             input: './src/index.cjs.js',
@@ -56,19 +50,15 @@ function build_min() {
         }))
         .pipe(uglify())
         .pipe(concat('feature-toggle-api.min.js'))
-        //    .pipe(sourcemaps.write())
         .pipe(dest('./'));
 };
 
 function build_plugins() {
     return src(['src/plugins/htmlplugin/plugin-html.js', 'src/plugins/urlplugin/plugin-url.js'])
-        //   .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ["@babel/preset-env"]
         }))
         .pipe(uglify())
-        //.pipe(concat('feature-toggle-api.min.js'))
-        //    .pipe(sourcemaps.write())
         .pipe(dest('./'));
 };
 
